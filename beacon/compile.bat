192d75116c5c913b3492ba7815608518419579b7
gw6pfk@gmail.com <gw6pfk@gmail.com@658ee4c5-b13f-2da0-0187-74ae307cd233>
2014-03-17 19:04:45 +0000

    works

--- beacon/compile.bat
+++ beacon/compile.bat
@@ -1,10 +1,13 @@
-sdcc.exe  -mpic14 -p16f876 -D__16f876 -I"C:\Program Files (x86)\SDCC\include" -I"C:\Program Files (x86)\SDCC\non-free\include" -I"C:\Program Files (x86)\gputils\bin" -c simple.c 
+rem sdcc.exe  -V -mpic14 -p16f876 -D__16f876 -I"C:\Program Files (x86)\SDCC\include" -I"C:\Program Files (x86)\SDCC\non-free\include" -I"C:\Program Files (x86)\gputils\bin" -c simple.c 
 
-rem sdcc -V --use-non-free -mpic14 -p16f877 -c simple.c
+
+sdcc -V --use-non-free -mpic14 -p16f876 simple.c
+
 
 
 rem sdcc -S -V -mpic14 -p16f876 -D__16f876 --use-non-free simple.c
-rem sdcc.exe  -mpic14 -p16f876 -D__16f876  --out-fmt-ihx -I"C:\Program Files (x86)\SDCC\include" -I"C:\Program Files (x86)\SDCC\non-free\include" -I"C:\Program Files (x86)\gputils\bin" -c simple.c -o simple.hex
+rem sdcc.exe  -mpic14 -p16f876 -D__16f876  --out-fmt-ihx -I"C:\Program Files (x86)\SDCC\include" -I"C:\Program Files (x86)\SDCC\non-free\include" -I"C:\Program Files (x86)\gputils\bin" -c simple.c 
+
 
 
 

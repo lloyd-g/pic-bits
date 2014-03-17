83a493587eb7aa366273502544ef77b613f4061b
gw6pfk@gmail.com <gw6pfk@gmail.com@658ee4c5-b13f-2da0-0187-74ae307cd233>
2014-03-17 11:11:23 +0000

    comment last one works and puts it in ASM file on configs this is from manual why others are quoted on web don't know?

--- beacon/simple.c
+++ beacon/simple.c
@@ -7,6 +7,7 @@
 //    typedef unsigned int  config;
 //    config at 0x2007 __CONFIG = _HS_OSC & _PWRTE_ON & _BODEN_OFF & _WDT_OFF & _LVP_OFF;
 // unsigned int config at 0x2007 __CONFIG = _HS_OSC & _PWRTE_ON & _BODEN_OFF & _WDT_OFF & _LVP_OFF;
+// last one works and puts it in ASM file
 static __code uint16_t __at (0x2007)  config  =  _HS_OSC & _PWRTE_ON & _BODEN_OFF & _WDT_OFF & _LVP_OFF;
 
 unsigned int delayCount = 0;

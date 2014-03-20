550d3dec40c49382c69d3b0e205a812feefd37ff
gw6pfk@gmail.com <gw6pfk@gmail.com@658ee4c5-b13f-2da0-0187-74ae307cd233>
2014-03-20 13:46:05 +0000

    start point

---
+++ timer/int-test.c
@@ -0,0 +1,41 @@
+#include <pic14\pic16f876.h>
+#include <stdint.h> //Needed for uint16_t
+
+// last one works and puts it in ASM file
+static __code uint16_t __at (0x2007)  config  =  _HS_OSC & _PWRTE_ON & _BODEN_OFF & _WDT_OFF & _LVP_OFF;
+// intrupt toggals PORTB
+static void isr(void) interrupt 0
+ { 
+T0IF = 0; /* Clear timer interrupt flag */ 
+PORTB = ~PORTB; /* toggals PORTB think its all bits */
+ }
+
+
+
+
+
+// Main body
+void main(void) {
+
+    CMCON = 0x07;           /* disable comparators */
+    T0CS = 0;               /* clear to enable timer mode */
+    PSA = 0;                /* clear to assign prescaller to TMRO */
+
+    TRISB &= 0x00;           /* PORTB.0-3 Output, */
+    PORTB = 0x00;           /* PORTB.0-3 to zero, */
+
+    PS2 = 0;                /* 001 @ 4Mhz = 1.024 mS */
+    PS1 = 0;  
+    PS0 = 1;  
+
+    INTCON = 0;             /* clear interrupt flag bits */
+    GIE = 1;                /* global interrupt enable */
+    T0IE = 1;               /* TMR0 overflow interrupt enable */
+      
+    TMR0 = 0;               /* clear the value in TMR0 */
+while(1)
+ { 
+ /* Loop forever */
+ /* We aren't doing anything in the main loop... we're just sittin' here like a jackass waiting for that TMR0 interrupt to occur. */ 
+ } 
+}

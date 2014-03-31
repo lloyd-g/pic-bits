6f090c15830858930842981f67612ce6d57e9da7
gw6pfk@gmail.com <gw6pfk@gmail.com@658ee4c5-b13f-2da0-0187-74ae307cd233>
2014-03-31 18:22:39 +0000

    sim?

--- serial/sim.bat
+++ serial/sim.bat
@@ -1,3 +1,4 @@
-sdcc -V --use-non-free --debug -mpic14 -p16f876 int-serial.c
-gpsim -pp16f876 -s int-serial.cod int-serial.asm
+//sdcc -V --use-non-free --debug -mpic14 -p16f876 int-serial.c
+//gpsim -pp16f876 -s int-serial.cod int-serial.asm
+gpsim -pp16f876 -c suart.stc
 cmd

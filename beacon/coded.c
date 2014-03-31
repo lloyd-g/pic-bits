b5a7236844d1f20183b9fdb71b4f644615be7edd
gw6pfk@gmail.com <gw6pfk@gmail.com@658ee4c5-b13f-2da0-0187-74ae307cd233>
2014-03-31 15:09:17 +0000

    funcions

--- beacon/coded.c
+++ beacon/coded.c
@@ -1,10 +1,51 @@
 #include <pic14\pic16f876.h>
 #include <stdint.h> //Needed for uint16_t
-
+#define DIT 2000
+#define DA  6000
 // Configurations
 static __code uint16_t __at (0x2007)  config  =  _HS_OSC & _PWRTE_ON & _BODEN_OFF & _WDT_OFF & _LVP_OFF;
 
 unsigned int delayCount = 0;
+void dit()
+     {
+          while ( delayCount < DIT ) // Delay Loop
+                    {
+                    delayCount++;
+                    }
+          delayCount = 0; // reset counter
+          // port off
+          PORTB = 0x00;
+
+          // delay while off
+          while ( delayCount < DIT ) // Delay Loop
+                    {
+                    delayCount++;
+                    }
+          delayCount = 0; // reset counter
+          // port on
+          PORTB = 0x01;
+     }
+void da()
+     {
+          while ( delayCount < DA ) // Delay Loop
+                    {
+                    delayCount++;
+                    }
+          delayCount = 0; // reset counter
+          // port off
+          PORTB = 0x00;
+
+          // delay while off
+          while ( delayCount < DA ) // Delay Loop
+                    {
+                    delayCount++;
+                    }
+          delayCount = 0; // reset counter
+          // port on
+          PORTB = 0x01;
+     }
+
+
 // Main body
 void main() {
 
@@ -20,23 +61,8 @@ void main() {
     PORTB = 0x01;
 // keep repeating
     while(1) {
-    // delay while on
-       while ( delayCount < 4000 ) // Delay Loop
-          {
-           delayCount++;
-          }
-          delayCount = 0; // reset counter 
-     // port off     
-     PORTB = 0x00;
-            
-// delay while off
-       while ( delayCount < 4000 ) // Delay Loop
-          {
-           delayCount++;
-          }
-          delayCount = 0; // reset counter 
-     // port on     
-     PORTB = 0x01;
- 
+     dit();
+     da();
     }
 }
+

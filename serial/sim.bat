f143a6150fad02728f086cd54b04d5727bed2c00
gw6pfk@gmail.com <gw6pfk@gmail.com@658ee4c5-b13f-2da0-0187-74ae307cd233>
2014-04-01 19:45:44 +0000

    18f/main.c

--- serial/sim.bat
+++ serial/sim.bat
@@ -1,4 +1,4 @@
 //sdcc -V --use-non-free --debug -mpic14 -p16f876 int-serial.c
 //gpsim -pp16f876 -s int-serial.cod int-serial.asm
-gpsim -pp16f876 -c suart.stc
+gpsim -pp16f876 -s 3-uart_tx.cod -c suart.stc
 cmd

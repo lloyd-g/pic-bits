b5d76b402ec295556fc87ad1b9014415b24f4f56
gw6pfk@gmail.com <gw6pfk@gmail.com@658ee4c5-b13f-2da0-0187-74ae307cd233>
2014-04-07 15:02:02 +0000

    env

--- serial/sim.bat
+++ serial/sim.bat
@@ -1,4 +1,5 @@
-//sdcc -V --use-non-free --debug -mpic14 -p16f876 int-serial.c
-//gpsim -pp16f876 -s int-serial.cod int-serial.asm
-gpsim -pp16f876 -s 3-uart_tx.cod -c suart.stc
+rem sdcc -V --use-non-free --debug -mpic14 -p16f876 int-serial.c
+rem gpsim -pp16f876 -s int-serial.cod int-serial.asm
+gpsim -c env.conf -s 3-uart_tx.cod 
+rem gpsim -pp16f876 -s 3-uart_tx.cod -c suart.stc > scope.ch0 = "portc6"
 cmd

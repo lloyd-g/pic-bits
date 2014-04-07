9cbfe400a30d4d406ccd3563c4ec544f6c523050
gw6pfk@gmail.com <gw6pfk@gmail.com@658ee4c5-b13f-2da0-0187-74ae307cd233>
2014-04-07 14:10:37 +0000

    need to set up PWM output to give 50% duty for on and 0% for off?

---
+++ beacon/buzz.c
@@ -0,0 +1,77 @@
+#include <pic14\pic16f876.h>
+ 
+#include <stdint.h> //Needed for uint16_t
+ 
+
+ 
+//#pragma config CP0=OFF,OSCS=ON,OSC=LP,BOR=ON,BORV=25,WDT=ON,WDTPS=128,CCP2MUX=ON
+ 
+//#pragma config STVR=ON
+ 
+// Configurations
+ 
+//    typedef unsigned int  config;
+ 
+//    config at 0x2007 __CONFIG = _HS_OSC & _PWRTE_ON & _BODEN_OFF & _WDT_OFF & _LVP_OFF;
+ 
+// unsigned int config at 0x2007 __CONFIG = _HS_OSC & _PWRTE_ON & _BODEN_OFF & _WDT_OFF & _LVP_OFF;
+ 
+// last one works and puts it in ASM file
+ 
+static __code uint16_t __at (0x2007)  config  =  _HS_OSC & _PWRTE_ON & _BODEN_OFF & _WDT_OFF & _LVP_OFF;
+ 
+
+ 
+unsigned int delayCount = 0;
+ 
+// Main body
+ 
+void main() {
+ 
+
+ 
+    // Initializing ports
+ 
+    PORTA = 0;
+ 
+    PORTB = 0;
+ 
+
+ 
+    // Set RA4 as input and RB0 as output
+ 
+    TRISA |= 0x10;
+ 
+    TRISB &= 0x00;
+ 
+
+ 
+    // Set value 0x01 to PORTB
+ 
+    PORTB = 0x01;
+ 
+
+ 
+    // If button is pressed, toggle PORTB
+ 
+    while(1) {
+ 
+      while ( delayCount < 4000 ) // Delay Loop
+ 
+                {
+ 
+                        delayCount++;
+ 
+                }
+ 
+        //if(RA4 != 0)
+ 
+            PORTB = ~PORTB;
+ 
+            delayCount = 0; // reset counter
+ 
+ 
+ 
+    }
+ 
+} 

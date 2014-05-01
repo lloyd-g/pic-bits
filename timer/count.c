432bf8a9b4fbd3ad8c0525542178c6318a1d4f01
gw6pfk@gmail.com <gw6pfk@gmail.com@658ee4c5-b13f-2da0-0187-74ae307cd233>
2014-05-01 11:34:36 +0000

    anotate

--- timer/count.c
+++ timer/count.c
@@ -3,6 +3,8 @@
 
 // last one works and puts it in ASM file
 static __code uint16_t __at (0x2007)  config  =  _HS_OSC & _PWRTE_ON & _BODEN_OFF & _WDT_OFF & _LVP_OFF;
+
+
 // intrupt 
 void Intr(void) __interrupt 0 
  { 
@@ -10,18 +12,14 @@ T0IF = 0; /* Clear timer interrupt flag */
 PORTA = PORTA + 1; // count will role over but should show as contiuouse?
 }
 
-
-
-
-
 // Main body
 void main(void) {
 
     T0CS = 0;               /* clear to enable timer mode */
     PSA = 0;                /* clear to assign prescaller to TMRO */
 
-    TRISA &= 0x00;           /* PORTB.0-3 Output, */
-    PORTA = 0x00;           /* PORTB.0-3 to zero, */
+    TRISA &= 0x00;           /* PORTA.0-5 Output, */
+    PORTA = 0x00;           /* PORTA.0-5 to zero, */
 
     PS2 = 1;                /* 111 @ *Mhz = 33 mS 1:256 1khz */
     PS1 = 1;  
@@ -35,6 +33,6 @@ void main(void) {
 while(1)
  { 
  /* Loop forever */
- /* We aren't doing anything in the main loop... we're just sittin' here like a jackass waiting for that TMR0 interrupt to occur. */ 
+ 
  } 
 }
